dbd7c2f82aa94d32284320d8dde3ddc7ca6ccd73
Ninilini <janina.nicolini@tu-dortmund.de>
2018-04-23 17:07:59 +0200

    Messdaten aus dem Heft, Pythonskript

--- V14 Tomographie/plots/plot.py
+++ V14 Tomographie/plots/plot.py
@@ -1,93 +1,229 @@
 # coding=utf-8
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-import uncertainties.unumpy as unp
-from uncertainties import ufloat
-import sympy
-from uncertainties import correlated_values, correlation_matrix
-import scipy.integrate as int
-from uncertainties.unumpy import nominal_values as noms
-from uncertainties.unumpy import std_devs as sdevs
-import scipy.constants as con
-from scipy.constants import physical_constants as pcon
-#Table Funktion
-dummy = ufloat(69,42)
-dummyarray = np.array([dummy,dummy*6.626])
-udummyarray = unp.uarray([4], [5*con.pi])
+import pandas as pd
 
-#data1 = [a,b,c,d,e]
-#p1 = {'name':'tabelle1.tex','data':data1}
-#table(**p1)
-def table(name, data):
-	j=0
-	i=0
-	f = [0,0,0,0,0,0,0,0,0,0,0,0,0]
-	for i in range(len(data)):
-		if(type(data[i][0]) == type(dummy) or type(data[i][0]) == type(dummyarray[1]) or type(data) == type(udummyarray)):
-			f[i] = True
-		else:
-			f[i] = False
-	print(f)
-	output = open(name, 'w')
-	output.write(r'\begin{table}[h]' + '\n' + r'\centering' + '\n' + r'\caption{CAPTION}' + '\n' +r'\sisetup{%uncertainty-seperator = {\,},'+'\n'+r'table-number-alignment = center,'+'\n'+'table-unit-alignment = center,'+'\n'+'%table-figures-integer = 1,'+'\n'+'%table-figures-decimal = 1,'+'\n'+'table-auto-round = true'+'\n'+'}'+'\n'+ r'\begin{tabular}{ ')
-	for i in range(len(data)):
-		if(f[i]):
-			output.write(r'S[table-format= 3.1]'+'\n'+' @{\,$\pm{}$\,} '+'\n' + r' S[table-format= 3.1] ')
-		else:
-			output.write(r' S[table-format= 3.1] '+'\n')
-	output.write(r'}' + '\n' + r'\toprule' + '\n')
+np.set_printoptions(precision=4)
 
-	for i in range(len(data)):
-		if(i < (len(data)-1)):
-			if(f[i]):
-				output.write(r'\multicolumn{2}{c}{TITLE}'+'\n'+'&')
-			else:
-				output.write(r'{$\text{Title}$}'+'\n'+'&')
-		else:
-			if(f[i]):
-				output.write(r'\multicolumn{2}{c}{TITLE} \\'+'\n')
-			else:
-				output.write(r'{$\text{Title}$} \\'+'\n')
-	output.write(r' \midrule' + '\n')
+# Dateneinlesen
+counts = np.genfromtxt('../data/Alu_leer/Alu_l_2.Spe', skip_header=12,
+                       skip_footer=16)
+counts_0 = np.genfromtxt('../data/leer.Spe', skip_header=12,
+                         skip_footer=16)
+I_l, c_l, t_l = np.genfromtxt('../data/Alu_l.txt', unpack=True)
+I_2, c_2, t_2 = np.genfromtxt('../data/W2.txt', unpack=True)
+I_3, c_3, t_3 = np.genfromtxt('../data/W3.txt', unpack=True)
+I_5, c_5, t_5 = np.genfromtxt('../data/W5.txt', unpack=True)
 
-	#Tabelle
-	for j in range(len(data[0])):
-		i = 0
-		while i <= len(data)-1:
-				if(f[i]):
-					if(i == len(data)-1):
-						output.write(str(data[i][j].n) + '&' + str(data[i][j].s) + r'\\' + '\n')
-					else:
-						output.write(str(data[i][j].n)+ '&' + str(data[i][j].s) + '&')
-					i = i+1
-				else:
-					if(i == len(data)-1):
-						output.write(str(data[i][j]) + r'\\' + '\n')
-					else:
-						output.write(str(data[i][j]) + '&')
-					i = i+1
-	#Tabelle Ende
-	output.write(r'\bottomrule' + '\n' + r'\end{tabular}' + '\n' + r'\label{tab:LABEL}' + '\n' + r'\end{table}')
-	output.close()
+n = {'leer': counts, 'error_leer': np.sqrt(counts), 'null': counts_0,
+    'error_null': np.sqrt(counts_0)}
+df = pd.DataFrame(n)
+
+# Geometriematrix
+b = np.sqrt(2)
+
+# A wird ja mit den Absorptionskoeffizienten multipliziert, um dann die Raten
+# auszugeben. Demnach entsprechen die Komponenten der einzelnen Zeilen
+# den Strecken durch die Unter-Würfel, die bei der jeweiligen Projektion
+# durchstrahlt werden. Daher lassen sich die Zeilen bei uniformer Verteilung
+# auch aufsummieren (wie bei Würfel 2 und 3).
+A = np.matrix([[1, 1, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 1, 1, 1, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 1, 1],
+               [1, 0, 0, 1, 0, 0, 1, 0, 0],
+               [0, 1, 0, 0, 1, 0, 0, 1, 0],
+               [0, 0, 1, 0, 0, 1, 0, 0, 1],
+               [b, 0, 0, 0, 0, 0, 0, 0, 0],
+               [0, b, 0, b, 0, 0, 0, 0, 0],
+               [0, 0, b, 0, b, 0, b, 0, 0],
+               [0, 0, 0, 0, 0, b, 0, b, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0, b],
+               [0, 0, 0, 0, 0, 0, b, 0, 0],
+               [0, 0, 0, b, 0, 0, 0, b, 0],
+               [b, 0, 0, 0, b, 0, 0, 0, b],
+               [0, b, 0, 0, 0, b, 0, 0, 0],
+               [0, 0, b, 0, 0, 0, 0, 0, 0]])
+
+# Geoimetriematrix für Würfel 2 (vollständig aus Aluminium)
+A_2 = np.matrix([[3],
+                 [3*b],
+                 [2*b],
+                 [b]])
+# Geoimetriematrix für Würfel 3 (vollständig aus Blei)
+A_2 = np.matrix([[3],
+                 [2*b],
+                 [b]])
 
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+def kleinsteQuadrate(y, W, A):
+    temp = np.dot(np.linalg.inv(np.dot(A.T, np.dot(W, A))), A.T)
+    a = np.dot(temp, np.dot(W, y))
+    a_err = np.linalg.inv(np.dot(A.T, np.dot(W, A)))
+    return a, np.sqrt(np.diag(a_err))
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+# Alu_leer
+plt.bar(range(0, 511), df['leer'], yerr=df['error_leer'])
+plt.xlim(0, 120)
+plt.title('Messung des leeren Würfels')
+plt.xlabel('Kanal')
+plt.ylabel('Ereignisse')
+plt.savefig('plots/Alu_leer.pdf')
+plt.close()
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('plots/plot.pdf')
+print('Spektrum der Alu_leer geplottet...')
+
+# Leer
+
+plt.bar(range(0, 511), df['null'], yerr=df['error_null'])
+plt.xlim(0, 120)
+plt.title('Messung ohne Würfel')
+plt.xlabel('Kanal')
+plt.ylabel('Ereignisse')
+plt.savefig('plots/Leer.pdf')
+plt.close()
+
+print('(Zusätzlich: Spektrum der Leer geplottet...)')
+
+c_no = 13708
+t_no = 74.46  # s
+rate_no = c_no / t_no
+
+print('Messung ohne Würfel mit Rate: {:2f} +- {:2f} counts/s'
+      .format(rate_no, np.sqrt(c_no)/t_no))
+
+# I_0s für die Projektionen 2, 9, 10, 11
+rate_l = c_l / t_l
+err_rate_l = np.sqrt(c_l) / t_l
+
+# Würfel 2 (2, 9, 10, 11)
+rate_2 = c_2 / t_2
+err_rate_2 = np.sqrt(c_2) / t_2
+
+
+# Würfel 3 (2, 9, 11)
+rate_3 = c_3 / t_3
+err_rate_3 = np.sqrt(c_3) / t_3
+
+
+# Würfel 5 (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+rate_5 = c_5 / t_5
+err_rate_5 = np.sqrt(c_5) / t_5
+
+
+# Mitteln für leeren Würfel
+I_leer = np.array(np.zeros(len(rate_5)))
+err_I_leer = np.array(np.zeros(len(rate_5)))
+
+for i in range(len(rate_5)):
+    if i < 6:
+        I_leer[i] = (rate_l[0] + rate_l[1]) / 2
+        err_I_leer[i] = (err_rate_l[0] + err_rate_l[1]) / 2
+    if (i == 6) or (i == 8) or (i == 9) or (i == 11):
+        I_leer[i] = rate_l[4]
+        err_I_leer[i] = err_rate_l[4]
+    if (i == 7) or (i == 10):
+        I_leer[i] = (rate_l[2] + rate_l[3]) / 2
+        err_I_leer[i] = (err_rate_l[2] + err_rate_l[3]) / 2
+
+# Umrechnen in ln(I_0/N_j)
+# Proj_1 steht für die Projektionen, die bei Würfel 2 und 3 verwendet wurden
+I_Proj_1 = np.array([I_leer[1], I_leer[4], I_leer[7], I_leer[10]])
+err_I_Proj_1 = np.array([err_I_leer[1],
+                         err_I_leer[4], err_I_leer[7], err_I_leer[10]])
+
+I_2 = np.log(I_Proj_1 / rate_2)
+I_3 = np.log(I_Proj_1 / rate_3)
+I_5 = np.log(I_leer / rate_5)
+
+err_I_2 = np.sqrt((np.sqrt(rate_2) / rate_2)**2 + (err_I_Proj_1 / I_Proj_1)**2)
+err_I_3 = np.sqrt((np.sqrt(rate_3) / rate_3)**2 + (err_I_Proj_1 / I_Proj_1)**2)
+err_I_5 = np.sqrt((np.sqrt(rate_5) / rate_5)**2 + (err_I_leer / I_leer)**2)
+
+
+print('''
+    ###############################################################
+
+    ~~~ Raten der verschiedenen Würfel ~~~
+
+    Alu_leer: (Projektionen 2, 9, 10, 11)
+    -----------------------------------------------
+    Werte = {}
+    Fehler = {}
+
+    Alu_leer mit Mittelung: (alle 12 Projektionen)
+    -----------------------------------------------
+    Werte = {}
+    Fehler = {}
+
+    Würfel 2: (Projektionen 2, 9, 10, 11)
+    -----------------------------------------------
+    Werte = {}
+    Fehler = {}
+
+    Würfel 3: (Projektionen 2, 10, 11)
+    -----------------------------------------------
+    Werte = {}
+    Fehler = {}
+
+    Würfel 5: (alle 12 Projektionen)
+    -----------------------------------------------
+    Werte = {}
+    Fehler = {}
+
+    ###############################################################
+    '''
+      .format(rate_l, err_rate_l, I_leer, err_I_leer, rate_2, err_rate_2,
+              rate_3, err_rate_3, rate_5, err_rate_5))
+
+print('''
+    ~~~ Logarithmen der Raten der verschiedenen Würfel ~~~
+
+    Würfel 2:
+    -----------------------------------------------
+    Werte = {}
+    Fehler = {}
+
+    Würfel 3:
+    -----------------------------------------------
+    Werte = {}
+    Fehler = {}
+
+    Würfel 5:
+    -----------------------------------------------
+    Werte = {}
+    Fehler = {}
+
+    ###############################################################
+    '''.format(I_2, err_I_2, I_3, err_I_3, I_5, err_I_5))
+
+# Gewichtungsmatrizen
+W_2 = np.diag(1/err_I_2**2)
+W_3 = np.diag(1/err_I_3**2)
+W_5 = np.diag(1/err_I_5**2)
+
+# Über kleinsteQuadrate mu berechnen
+mu_2, err_mu_2 = kleinsteQuadrate(I_2, W=W_2, A=A_2)
+mu_3, err_mu_3 = kleinsteQuadrate(I_3, W=W_3, A=A_3)
+mu_5, err_mu_5 = kleinsteQuadrate(I_5, W=W_5, A=A)
+
+print(
+      '''
+          ~~~ Absorptionskoeffizienten der verschiedenen Würfel ~~~
+
+          Würfel 2:
+          -----------------------------------------------
+          Werte = {}
+          Fehler = {}
+
+          Würfel 3:
+          -----------------------------------------------
+          Werte = {}
+          Fehler = {}
+
+          Würfel 5:
+          -----------------------------------------------
+          Werte = {}
+          Fehler = {}
+          '''.format(mu_2, err_mu_2, mu_3, err_mu_3, mu_5, err_mu_5)
+      )
