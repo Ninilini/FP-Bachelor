9c9de843def032201f4e2b12e058a2f2060a80cb
Ninilini <janina.nicolini@tu-dortmund.de>
2018-04-24 15:28:02 +0200

    Auswertungsskript läuft

--- V14 Tomographie/plots/plot.py
+++ V14 Tomographie/plots/plot.py
@@ -6,18 +6,20 @@ import pandas as pd
 np.set_printoptions(precision=4)
 
 # Dateneinlesen
-counts = np.genfromtxt('../data/Alu_leer/Alu_l_2.Spe', skip_header=12,
-                       skip_footer=16)
+counts = np.genfromtxt('../data/Alu_leer/alu_l_2.Spe', skip_header=12,
+                       skip_footer=17)
 counts_0 = np.genfromtxt('../data/leer.Spe', skip_header=12,
-                         skip_footer=16)
+                         skip_footer=17)
 I_l, c_l, t_l = np.genfromtxt('../data/Alu_l.txt', unpack=True)
 I_2, c_2, t_2 = np.genfromtxt('../data/W2.txt', unpack=True)
 I_3, c_3, t_3 = np.genfromtxt('../data/W3.txt', unpack=True)
 I_5, c_5, t_5 = np.genfromtxt('../data/W5.txt', unpack=True)
 
-n = {'leer': counts, 'error_leer': np.sqrt(counts), 'null': counts_0,
-    'error_null': np.sqrt(counts_0)}
-df = pd.DataFrame(n)
+leer = counts
+error_leer = np.sqrt(counts)
+null = counts_0
+error_null = np.sqrt(counts_0)
+# df = pd.DataFrame(n)
 
 # Geometriematrix
 b = np.sqrt(2)
@@ -50,7 +52,7 @@ A_2 = np.matrix([[3],
                  [2*b],
                  [b]])
 # Geoimetriematrix für Würfel 3 (vollständig aus Blei)
-A_2 = np.matrix([[3],
+A_3 = np.matrix([[3],
                  [2*b],
                  [b]])
 
@@ -63,24 +65,24 @@ def kleinsteQuadrate(y, W, A):
 
 
 # Alu_leer
-plt.bar(range(0, 511), df['leer'], yerr=df['error_leer'])
-plt.xlim(0, 120)
+plt.bar(range(0, 511), leer, yerr=error_leer)
+plt.xlim(0, 250)
 plt.title('Messung des leeren Würfels')
 plt.xlabel('Kanal')
 plt.ylabel('Ereignisse')
-plt.savefig('plots/Alu_leer.pdf')
+plt.savefig('Alu_leer.pdf')
 plt.close()
 
 print('Spektrum der Alu_leer geplottet...')
 
 # Leer
 
-plt.bar(range(0, 511), df['null'], yerr=df['error_null'])
-plt.xlim(0, 120)
+plt.bar(range(0, 511), null, yerr=error_null)
+plt.xlim(0, 250)
 plt.title('Messung ohne Würfel')
 plt.xlabel('Kanal')
 plt.ylabel('Ereignisse')
-plt.savefig('plots/Leer.pdf')
+plt.savefig('Leer.pdf')
 plt.close()
 
 print('(Zusätzlich: Spektrum der Leer geplottet...)')
@@ -116,28 +118,33 @@ I_leer = np.array(np.zeros(len(rate_5)))
 err_I_leer = np.array(np.zeros(len(rate_5)))
 
 for i in range(len(rate_5)):
-    if i < 6:
-        I_leer[i] = (rate_l[0] + rate_l[1]) / 2
-        err_I_leer[i] = (err_rate_l[0] + err_rate_l[1]) / 2
-    if (i == 6) or (i == 8) or (i == 9) or (i == 11):
-        I_leer[i] = rate_l[4]
-        err_I_leer[i] = err_rate_l[4]
-    if (i == 7) or (i == 10):
-        I_leer[i] = (rate_l[2] + rate_l[3]) / 2
-        err_I_leer[i] = (err_rate_l[2] + err_rate_l[3]) / 2
+    if (i == 9) or (i == 14):
+        I_leer[i] = rate_l[1]
+        err_I_leer[i] = err_rate_l[1]
+    if i < 7:
+        I_leer[i] = rate_l[0]
+        err_I_leer[i] = err_rate_l[0]
+    if (i == 13) or (i == 8) or (i == 15) or (i == 10):
+        I_leer[i] = rate_l[2]
+        err_I_leer[i] = err_rate_l[2]
+    if (i == 7) or (i == 11) or (i == 12) or (i == 16):
+        I_leer[i] = rate_l[3]
+        err_I_leer[i] = err_rate_l[3]
 
 # Umrechnen in ln(I_0/N_j)
 # Proj_1 steht für die Projektionen, die bei Würfel 2 und 3 verwendet wurden
-I_Proj_1 = np.array([I_leer[1], I_leer[4], I_leer[7], I_leer[10]])
-err_I_Proj_1 = np.array([err_I_leer[1],
-                         err_I_leer[4], err_I_leer[7], err_I_leer[10]])
+I_Proj_2 = np.array(rate_l)
+err_I_Proj_2 = np.array(err_rate_l)
 
-I_2 = np.log(I_Proj_1 / rate_2)
-I_3 = np.log(I_Proj_1 / rate_3)
+I_Proj_3 = np.array([rate_l[0],rate_l[2],rate_l[3]])
+err_I_Proj_3 = np.array([err_rate_l[0],err_rate_l[2],err_rate_l[3]])
+
+I_2 = np.log(I_Proj_2 / rate_2)
+I_3 = np.log(I_Proj_3 / rate_3)
 I_5 = np.log(I_leer / rate_5)
 
-err_I_2 = np.sqrt((np.sqrt(rate_2) / rate_2)**2 + (err_I_Proj_1 / I_Proj_1)**2)
-err_I_3 = np.sqrt((np.sqrt(rate_3) / rate_3)**2 + (err_I_Proj_1 / I_Proj_1)**2)
+err_I_2 = np.sqrt((np.sqrt(rate_2) / rate_2)**2 + (err_I_Proj_2 / I_Proj_2)**2)
+err_I_3 = np.sqrt((np.sqrt(rate_3) / rate_3)**2 + (err_I_Proj_3 / I_Proj_3)**2)
 err_I_5 = np.sqrt((np.sqrt(rate_5) / rate_5)**2 + (err_I_leer / I_leer)**2)
 
 
@@ -166,7 +173,7 @@ print('''
     Werte = {}
     Fehler = {}
 
-    Würfel 5: (alle 12 Projektionen)
+    Würfel 5: (alle {} Projektionen)
     -----------------------------------------------
     Werte = {}
     Fehler = {}
@@ -174,28 +181,29 @@ print('''
     ###############################################################
     '''
       .format(rate_l, err_rate_l, I_leer, err_I_leer, rate_2, err_rate_2,
-              rate_3, err_rate_3, rate_5, err_rate_5))
+              rate_3, err_rate_3, len(rate_5), rate_5, err_rate_5))
 
 print('''
-    ~~~ Logarithmen der Raten der verschiedenen Würfel ~~~
+~~~ Logarithmen der Raten der verschiedenen Würfel ~~~
 
-    Würfel 2:
-    -----------------------------------------------
-    Werte = {}
-    Fehler = {}
+Würfel 2:
+-----------------------------------------------
+Werte = {}
+Fehler = {}
 
-    Würfel 3:
-    -----------------------------------------------
-    Werte = {}
-    Fehler = {}
+Würfel 3:
+-----------------------------------------------
+Werte = {}
+Fehler = {}
 
-    Würfel 5:
-    -----------------------------------------------
-    Werte = {}
-    Fehler = {}
+Würfel 5:
+-----------------------------------------------
+Werte = {}
+Fehler = {}
 
-    ###############################################################
-    '''.format(I_2, err_I_2, I_3, err_I_3, I_5, err_I_5))
+###############################################################
+'''.format(I_2, err_I_2, I_3, err_I_3, I_5, err_I_5)
+)
 
 # Gewichtungsmatrizen
 W_2 = np.diag(1/err_I_2**2)
@@ -208,22 +216,22 @@ mu_3, err_mu_3 = kleinsteQuadrate(I_3, W=W_3, A=A_3)
 mu_5, err_mu_5 = kleinsteQuadrate(I_5, W=W_5, A=A)
 
 print(
-      '''
-          ~~~ Absorptionskoeffizienten der verschiedenen Würfel ~~~
+'''
+~~~ Absorptionskoeffizienten der verschiedenen Würfel ~~~
 
-          Würfel 2:
-          -----------------------------------------------
-          Werte = {}
-          Fehler = {}
+Würfel 2:
+-----------------------------------------------
+Werte = {}
+Fehler = {}
 
-          Würfel 3:
-          -----------------------------------------------
-          Werte = {}
-          Fehler = {}
+Würfel 3:
+-----------------------------------------------
+Werte = {}
+Fehler = {}
 
-          Würfel 5:
-          -----------------------------------------------
-          Werte = {}
-          Fehler = {}
-          '''.format(mu_2, err_mu_2, mu_3, err_mu_3, mu_5, err_mu_5)
-      )
+Würfel 5:
+-----------------------------------------------
+Werte = {}
+Fehler = {}
+'''.format(mu_2, err_mu_2, mu_3, err_mu_3, mu_5, err_mu_5)
+)
