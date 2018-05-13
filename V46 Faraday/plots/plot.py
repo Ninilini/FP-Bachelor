6ed83bd30c9aabf6988d2f159ef172fe596fe74a
Vanessa Sulaiman <vanessa.sulaiman@tu-dortmund.de>
2018-05-13 19:27:32 +0200

    Faraday Auswertung II

--- V46 Faraday/plots/plot.py
+++ V46 Faraday/plots/plot.py
@@ -10,7 +10,6 @@ l, th1, th2, th3 = np.genfromtxt('data/GaAs.txt', unpack=True)
 d1 = 0.00511
 d2 = 0.00136
 d3 = 0.001296
-l = l*(10**(-6))
 
 # Normierung auf Einheitslänge:
 th1 = th1/d1
@@ -39,7 +38,7 @@ print('Parameter: ', params1, '\nFehler: ', np.sqrt(np.diag(cov1)))
 plt.plot(l**2, th1, 'b.', label='hochreines GaAs')
 plt.plot(l**2, th2, 'r.', label=r'n-dotiertes GaAs mit $N = \SI{1.2e18}{cm^{-3}}$')
 plt.plot(l**2, th3, 'g.', label=r'n-dotiertes GaAs mit $N = \SI{2.8e18}{cm^{-3}}$')
-plt.xlabel(r'$\lambda^2 \:/\: \si{\meter^{2}}$')
+plt.xlabel(r'$\lambda^2 \:/\: \si{\micro\meter^{2}}$')
 plt.ylabel(r'$\frac{\theta}{d} \:/\: \si{\degree\meter^{-1}}$')
 plt.legend(loc='best')
 
@@ -52,26 +51,16 @@ plt.close()
 y1 = (th2-th1)*2*np.pi/360
 y2 = (th3-th1)*2*np.pi/360
 
-u1 = np.array([y1[0], y1[1], y1[2], y1[4], y1[5]])
-v1 = np.array([y1[3], y1[6], y1[7]])
-lu1 = np.array([l[0], l[1], l[2], l[4], l[5]])
-lv1 = np.array([l[3], l[6], l[7]])
-u2 = np.array([y2[1], y2[2], y2[3], y2[4]])
-v2 = np.array([y2[0], y2[5], y2[6], y2[7]])
-lu2 = np.array([l[1], l[2], l[3], l[4]])
-lv2 = np.array([l[0], l[5], l[6], l[7]])
 def f2(x, a, b):
 	return a*x + b
-params2, cov2 = curve_fit(f2, lu1**2, u1)
-params3, cov3 = curve_fit(f2, lu2**2, u2)
-l2 = np.linspace(10**(-12), 6.5*(10**(-12)))
-plt.plot(lu1**2, u1, 'r.', label=r'$N = \SI{1.2e18}{cm^{-3}}$ (Differenz der Messwerte)')
-plt.plot(lu2**2, u2, 'g.', label=r'$N = \SI{2.8e18}{cm^{-3}}$ (Differenz der Messwerte)')
-plt.plot(lv1**2, v1, 'rx')
-plt.plot(lv2**2, v2, 'gx')
+params2, cov2 = curve_fit(f2, l**2, y1)
+params3, cov3 = curve_fit(f2, l**2, y2)
+l2 = np.linspace(1, 6.5)
+plt.plot(l**2, y1, 'r.', label=r'$N = \SI{1.2e18}{cm^{-3}}$ (Differenz der Messwerte)')
+plt.plot(l**2, y2, 'g.', label=r'$N = \SI{2.8e18}{cm^{-3}}$ (Differenz der Messwerte)')
 plt.plot(l2, f2(l2, *params2), 'r-', label=r'$N = \SI{1.2e18}{cm^{-3}}$ (linearer Fit)')
 plt.plot(l2, f2(l2, *params3), 'g-', label=r'$N = \SI{2.8e18}{cm^{-3}}$ (linearer Fit)')
-plt.xlabel(r'$\lambda^2 \:/\: \si{\meter^{2}}$')
+plt.xlabel(r'$\lambda^2 \:/\: \si{\micro\meter^{2}}$')
 plt.ylabel(r'$\frac{\theta}{d} \:/\: \si{rad\,\meter^{-1}}$')
 plt.legend(loc='best')
 
@@ -84,17 +73,29 @@ print('Parameter: ', params2, '\nFehler: ', np.sqrt(np.diag(cov2)))
 print('Fit für N=2.8e18:')
 print('Parameter: ', params3, '\nFehler: ', np.sqrt(np.diag(cov3)))
 
-# x = np.linspace(0, 10, 1000)
-# y = x ** np.sin(x)
-#
-# plt.subplot(1, 2, 1)
-# plt.plot(x, y, label='Kurve')
-# plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-# plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-# plt.legend(loc='best')
-#
-# plt.subplot(1, 2, 2)
-# plt.plot(x, y, label='Kurve')
-# plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-# plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-# plt.legend(loc='best')
+########################################################
+
+a = (1.602176462**3)/(8*(np.pi**2)*8.854187817*(2.99792458**3))
+N1 = 1.2
+N2 = 2.8
+n = 3.354
+m_e = 0.910938188
+B_max = ufloat(408, 10)
+A1 = ufloat(8.75936855, 9.34901912)
+A2 = ufloat(9.35898797, 6.57753851)
+print('N = 1.2: m*=', unp.sqrt(a*N1*B_max/(n*A1))*m_e, ' m_e')
+print('N = 2.8: m*=', unp.sqrt(a*N2*B_max/(n*A2))*m_e, ' m_e')
+
+# Magnetfeld-Fit:
+# Parameter:  [ -1.31382591   2.99072852 405.30603237]
+# Fehler:  [ 0.07652639  0.90317205 10.12785969]
+
+# Fit für N=1.2e18:
+# Parameter:  [8.75936855 9.66626756]
+# Fehler:  [ 9.34901912 36.69455966]
+# Fit für N=2.8e18:
+# Parameter:  [ 9.35898797 13.2345978 ]
+# Fehler:  [ 6.57753851 25.81659948]
+
+# N = 1.2: m*= 0.055+/-0.029  m_e
+# N = 2.8: m*= 0.081+/-0.029  m_e
