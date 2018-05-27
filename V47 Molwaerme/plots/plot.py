53028731c37f97b3e34e3fb8b4d0f064c3f684a0
Janina Nicolini <janinasmac@dynip-129-217-106-112.wifi.tu-dortmund.de>
2018-05-27 22:16:10 +0200

    Fehlerrechnung Cv noch falsch

--- V47 Molwaerme/plots/plot.py
+++ V47 Molwaerme/plots/plot.py
@@ -3,13 +3,9 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-import uncertainties.unumpy as unp
-from uncertainties import ufloat
-import sympy
-from uncertainties import correlated_values, correlation_matrix
-import scipy.integrate as int
+from uncertainties import ufloat, unumpy
 from uncertainties.unumpy import nominal_values as noms
-from uncertainties.unumpy import std_devs as sdevs
+import scipy.integrate as int
 import scipy.constants as con
 from scipy.constants import physical_constants as pcon
 
@@ -26,15 +22,17 @@ b = params[1]
 dm = np.sqrt(cov[0][0])
 db = np.sqrt(cov[1][1])
 
-print(m)
-print(dm)
-print(b)
-print(db)
+m = ufloat(m, dm)
+b = ufloat(b, db)
+
+print('m = ', m)
+print('b = ', b)
+
 
 x = np.linspace(70, 300, 1000)
 
 plt.plot(1/T, alpha, 'rx', label='Messwerte')
-plt.plot(1/x, f(x, m, b), 'k-', label='Regressionsgerade')
+plt.plot(1/x, noms(f(x, m, b)), 'k-', label='Regressionsgerade')
 plt.grid()
 plt.xlabel(r"$T^{-1}$ in $\mathrm{K}^{-1}$")
 plt.ylabel(r"$\alpha\cdot 10^{-6}$ in $\mathrm{K}^{-1}$")
@@ -47,8 +45,49 @@ mcu = 342
 Mcu = 63.546
 k = 140
 V = 7.092*10**(-6)
-dt, U, I, T1, T2 = np.loadtxt('data/data.txt', unpack = True)
+dt2, U2, I2, R12, R22, T12, T22 = np.loadtxt('data/data.txt', unpack = True)
+dt = unumpy.uarray(dt2, 1)
+U = unumpy.uarray(U2, 0.01)
+I = unumpy.uarray(I2, 0.1)
+R1 = unumpy.uarray(R12, 0.1)
+R2 = unumpy.uarray(R22, 0.1)
 
-cp = (Mcu/mcu) * (U * I * dt)/(T2-T1)
+def t(R):
+    return 0.00134*R**2 + 2.296*R + 30.13
+
+T1 = t(R1)
+T2 = t(R2)
+
+print('''
+alpha:
+--------------------------
+''',f(T2, m, b))
+
+print('''
+T1:
+--------------------------
+''',T1)
+
+print('''
+T2:
+--------------------------
+''',T2)
+
+print('''
+dT:
+--------------------------
+''',T2-T1)
+
+cp = (Mcu/mcu) * (U * I/1000 * dt)/(T2-T1)
+
+print('''
+cp:
+--------------------------
+''',cp)
 
 cv = cp - (9 * f(T2, m, b)**2 * k * V * T2)
+
+print('''
+cv:
+--------------------------
+''',cv)
